fa09f06c6bbbf0dce7ba089459540ac8f6ee335f
Tomasz Gaweł <tomekgawel95@gmail.com>
2019-06-25 16:08:39 +0200

    Created database entities

---
+++ src/main/java/com/tomaszgawel/notez/core/components/entities/NoteEntity.java
@@ -0,0 +1,33 @@
+package com.tomaszgawel.notez.core.components.entities;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "notes")
+@Data
+@NoArgsConstructor
+public class NoteEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "creation_date")
+    private Date creationDate;
+
+    @Column(name = "modification_date")
+    private Date modificationDate;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    UserEntity user;
+
+}

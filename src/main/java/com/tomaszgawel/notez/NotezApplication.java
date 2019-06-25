01e73b240dd210d91bdc3c131273db0216d197d7
Tomasz <tomasz.j.gawel@gmail.com>
2019-06-25 14:44:09 +0200

    Initial commit

---
+++ src/main/java/com/tomaszgawel/notez/NotezApplication.java
@@ -0,0 +1,13 @@
+package com.tomaszgawel.notez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotezApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomaszgawel/notez/NotezApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tomaszgawel.notez;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NotezApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
